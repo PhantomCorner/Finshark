d593d10be83aae819f2f4cb195d0de127a977cb7
Jiao <jiaojiaobigbang@gmail.com>
2024-11-21 13:40:41 +0800

    init api file

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,7 +1,10 @@
 // import Card from "./components/card/card";
+import { searchCompanies } from "@/api";
 import CardList from "./components/cardList/cardList";
 import Search from "./components/search/search";
-export default function Home() {
+export default async function Home() {
+  const data = await searchCompanies("tsla");
+  console.log(data);
   return (
     <div>
       <Search />
