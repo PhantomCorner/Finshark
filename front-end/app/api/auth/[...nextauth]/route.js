6dceea674ee378b72f9e8f7f991426fa9b8e180a
PhantomCorner <jiaojiaobigbang@gmail.com>
2025-02-04 10:27:09 +1300

    init mogodb connection

--- front-end/app/api/auth/[...nextauth]/route.js
+++ front-end/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-
+import { connectToDb } from "@utils/database";
 console.log(process.env.GOOGLE_ID);
 const handler = NextAuth({
   providers: [
@@ -9,7 +9,18 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
-  async session({ session }) {},
+  async session({ session }) {
+    // check if a user exists
+
+    // if not, create a new one and save to db
+    try {
+      await connectToDb();
+      return true;
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+  },
   async signIn({ profile }) {},
 });
 export { handler as GET, handler as POST };
