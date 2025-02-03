3b03fad353dec1273fadde68de447c70b7eec448
PhantomCorner <jiaojiaobigbang@gmail.com>
2025-02-03 16:03:24 +1300

    added google auth content

---
+++ front-end/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+console.log(process.env.GOOGLE_ID);
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  async session({ session }) {},
+  async signIn({ profile }) {},
+});
+export { handler as GET, handler as POST };
