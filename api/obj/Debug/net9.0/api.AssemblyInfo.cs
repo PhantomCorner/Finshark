fd51e8921d63c0f96165098dc8cfdaccb9f36a7a
PhantomCorner <jiaojiaobigbang@gmail.com>
2025-05-13 21:20:44 +1200

    update

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93fe05d7e451ce66e06f80fc2fa3117ea816366f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2a30f01c97037faaf34ff85aa1dd40c80186f7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
