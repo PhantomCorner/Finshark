886cb0610a1a1dd645e652d9ae9066fe4d8463c8
PhantomCorner <jiaojiaobigbang@gmail.com>
2025-05-29 13:47:35 +1200

    add login page & form validation

--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -8,6 +8,7 @@ import IncomeStatement from "../Components/IncomeStatement/IncomeStatement";
 import DesignPage from "../Pages/DesignPage/DesignPage";
 import BalanceSheet from "../Components/BalanceSheet/BalanceSheet";
 import CashFlow from "../Components/CashFlow/CashFlow";
+import LoginPage from "../Pages/LoginPage/LoginPage";
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -26,6 +27,7 @@ export const router = createBrowserRouter([
         ],
       },
       { path: "design-guide", element: <DesignPage /> },
+      { path: "login", element: <LoginPage /> },
     ],
   },
 ]);
